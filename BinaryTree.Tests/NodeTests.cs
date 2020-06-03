5da598010989bc2579538ca219667eb7b1dee341
Michael Peterson <michpete@extendhealth.com>
2020-06-03 14:28:04 -0600

    Add binary tree node with in-order traversal

---
+++ BinaryTree.Tests/NodeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BinaryTree.Tests
+{
+    public class NodeTests
+    {
+        [Fact]
+        public void CanTraverseInOrder()
+        {
+            // arrange 
+            var root = new Node<string>("j"); 
+            // left sub-tree
+            root.Left = new Node<string>("f");
+            root.Left.Left = new Node<string>("a");
+            root.Left.Right = new Node<string>("h");
+            // right sub-tree
+            root.Right = new Node<string>("k");
+            root.Right.Right = new Node<string>("z");
+
+            // act
+            IEnumerable<string> values = root.ReadInOrder();
+
+            // assert
+            Assert.Equal(new []{"a", "f", "h", "j", "k", "z"}, values);
+        }
+    }
+}
---
+++ BinaryTree/Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree
+{
+    public class Node<T>
+    {
+        public Node(T val)
+        {
+            Val = val;
+        }
+
+        public T Val { get; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public IEnumerable<T> ReadInOrder()
+        {
+            var values = new List<T>();
+
+            void GetValuesInOrder(Node<T> node)
+            {
+                if(node.Left != null)
+                    GetValuesInOrder(node.Left);
+
+                values.Add(node.Val);
+
+                if(node.Right != null)
+                    GetValuesInOrder(node.Right);
+            }
+
+            GetValuesInOrder(this);
+
+            return values;
+        }
+    }
+}
